1cc9004fba15acaeda9fb3a73e37325646090797
bec <pihripi5@gmail.com>
2026-08-03 10:36:40 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-st.title("My stream lit app")
- names=st.text_input("Enter your name")
+st.title("My Page On  Streamlit application")
+
+name=st.text_input("Enter your name")
 
 if st.button("Submit"):
   st.write(f"Hello,{name}")
